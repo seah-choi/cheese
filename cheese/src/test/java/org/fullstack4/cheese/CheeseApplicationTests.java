4e7ba2f52e252ad1b55c7fe8a5a05b41eedcc003
DESKTOP-7DN2GF6\pc <leehv1234@kookmin.ac.kr>
2024-05-25 17:45:07 +0900

    etc: 프로젝트 생성

---
+++ cheese/src/test/java/org/fullstack4/cheese/CheeseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fullstack4.cheese;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheeseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
