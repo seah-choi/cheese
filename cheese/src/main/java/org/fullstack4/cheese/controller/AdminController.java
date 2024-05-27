36910071be0c8ee4a1c004728686cb5b50cefe19
seah <chltpdk120@naver.com>
2024-05-27 14:17:37 +0900

    admin

---
+++ cheese/src/main/java/org/fullstack4/cheese/controller/AdminController.java
@@ -0,0 +1,33 @@
+package org.fullstack4.cheese.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Log4j2
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/admin")
+public class AdminController {
+    @GetMapping("/list")
+    public void listGET(){
+
+    }
+
+    @GetMapping("/view")
+    public void viewGET(){
+
+    }
+
+    @GetMapping("/regist")
+    public void registGET(){
+
+    }
+
+    @GetMapping("/modify")
+    public void modifyGET(){
+
+    }
+}
